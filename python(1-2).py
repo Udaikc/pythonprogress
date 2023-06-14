c4685a7875f1ff9a3955018f49dfb6e95b65bfd3
Udaikc <124875797+Udaikc@users.noreply.github.com>
2023-06-14 20:13:54 +0530

    python progress 1-2

---
+++ python(1-2).py
@@ -0,0 +1,68 @@
+#day1
+
+#print("hello world!\nhello world!")
+#writing multiple codes in same line
+
+#print("hello" + " " + "udai")
+#string concatenation
+
+#s=input("what is your name?")
+#print(s)
+#input function
+
+#print(type(s))
+#trturning type
+
+#print("HELLO "+input("what is your name?") + " !")
+#print(len("udai")) len of the string
+
+#band generator
+#print("welcome to the band genertor")
+#city=input("which city did you grew up in\n")
+#pet=input("what is your pet name\n")
+#print("your band could be "+city+ " " + pet)
+
+#day2
+
+#type conversion
+#num_char=len(input("what is your name"))
+#new_num_char=str(num_char)
+#print("your name has " + new_num_char + " characters")
+
+#height=input("enter the height\n")
+#weight=input("enter the weight\n")
+#h=float(height)
+#w=int(weight)
+#bmi=(int(w))/(float(h ** 2))
+#print(int(bmi))
+#bmi =int(weight)/float(height) **2
+#print(int(bmi))
+
+#round of decimals
+#print(round(8/3))
+
+#rounding of numbers to two decimals places
+#print(round(8/3 , 3))
+
+#adding different variables in python
+#scores=0;
+#height=1.78
+#nit=True
+#print(f"the scores of height {height} is {scores} is {nit}")
+
+#alternate way of printing different variables
+#message =f"the scores of height {height} is {scores} is {nit}"
+#print(message)
+
+#day2 project tip generator
+#dol=input("what was the total bill ? $")
+#dollar=float(dol)
+#tip=input("what % of bill will you tip 10 ,12 , 15 !")
+#tip_int=int(tip)/100
+#bill_tip=dollar*tip_int
+#totalbill=bill_tip+dollar
+#pe=input("total number of people?")
+#int_pe=int(pe)
+#each_pe=totalbill/int_pe
+#print(round(each_pe,3))
+
